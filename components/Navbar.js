4a8783c05674712e40a3ef25d71a6d3d155f8bab
seovacmarko5 <seovacmarko5@gmail.com>
2022-11-11 17:23:39 +0100

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,12 @@ export default function Navbar() {
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/guides">Guides</Link></li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/banner.png" width={966} height={276}/>
       </div>
     </div>
   )
